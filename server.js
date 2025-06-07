647ff63447fc9de3e7d5a9b947184920f5bb0009
diodoarrahman <diodoarrahman2@gmail.com>
2025-06-07 19:04:01 +0700

    FIX bug home to regis

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const init = async () => {
         handler: {
             directory: {
                 path: '.',
-                index: ['pages/home.html'],
+                index: ['/pages/home.html'],
             },
         },
     });
